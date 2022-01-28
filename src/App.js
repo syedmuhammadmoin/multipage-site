8e13e5edd3a10e203837ac91ada023ac6c604a1f
Syed Moeen <syedmuhammadmoin@gmail.com>
2022-01-28 16:18:18 +0500

    components added with switch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // page components
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import About from './pageAbouts/About';
+import About from './pages/About';
 
 function App() {
   return (
@@ -12,17 +12,22 @@ function App() {
       <BrowserRouter>
         <nav>
           <h1>My Articles</h1>
+          <a href='/'>Home</a>
+          <a href='/about'>About</a>
+          <a href='/contact'>Contact</a>
         </nav>
 
-        <Route path='/'>
-          <Home />
-        </Route>
-        <Route path='/about'>
-          <About />
-        </Route>
-        <Route path='/contact'>
-          <Contact />
-        </Route>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+        </Switch>
       </BrowserRouter>
     </div>
   );
